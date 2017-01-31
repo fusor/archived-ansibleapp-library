d06aeddc32329663f97d1909e544a44005a21c83
Dylan Murray <dymurray@redhat.com>
2017-01-31 13:24:18 -0500

    Added ansibleapp-base

---
+++ ansibleapp-base/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ANSIBLEAPP_ACTION=$1
+
+playbooks=/ansibleapp/actions
+
+oc-login.sh
+
+if [[ "$ANSIBLEAPP_ACTION" == "provision" ]]; then
+  ansible-playbook $playbooks/provision.yaml
+elif [[ "$ANSIBLEAPP_ACTION" == "deprovision" ]]; then
+  ansible-playbook $playbooks/deprovision.yaml
+elif [[ "$ANSIBLEAPP_ACTION" == "bind" ]]; then
+  echo "BIND NOT IMPLEMENTED" # TODO
+elif [[ "$ANSIBLEAPP_ACTION" == "unbind" ]]; then
+  echo "UNBIND NOT IMPLEMENTED" # TODO
+fi
